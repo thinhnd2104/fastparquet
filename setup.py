5c499594ad022ca725bd1af5bb8af96d9308be5f
Martin Durant <martin.durant@utoronto.ca>
2021-05-09 16:37:00 -0400

    readd language_level

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ else:
     modules = [
         Extension(mod, fix_exts(sources))
         for mod, sources in modules_to_build.items()]
-    extra = {'ext_modules': cythonize(modules)}
+    extra = {'ext_modules': cythonize(modules, language_level=3)}
 
 install_requires = open('requirements.txt').read().strip().split('\n')
 
