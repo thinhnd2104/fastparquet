d440518e4aedae104a19c7834c99df1b8eb555a3
Martin Durant <martin.durant@utoronto.ca>
2021-05-13 16:54:16 -0400

    0.6.3

--- fastparquet/__init__.py
+++ fastparquet/__init__.py
@@ -1,5 +1,5 @@
 """parquet - read parquet files."""
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 
 from .thrift_structures import parquet_thrift
 from .core import read_thrift
--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ install_requires = open('requirements.txt').read().strip().split('\n')
 
 setup(
     name='fastparquet',
-    version='0.6.2',
+    version='0.6.3',
     description='Python support for Parquet file format',
     author='Martin Durant',
     author_email='mdurant@canaconda.com',
