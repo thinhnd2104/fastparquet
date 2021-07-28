de297550d59b0a70b9cffee08e1ebd5ba5a41cb8
Martin Durant <martin.durant@utoronto.ca>
2021-07-28 09:06:39 -0400

    Add cencoding to mock modules for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,12 +18,14 @@ import os
 import sys
 from unittest.mock import MagicMock
 
+
 class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
-            return MagicMock()
+        return MagicMock()
 
-MOCK_MODULES = ['fastparquet.speedups']
+
+MOCK_MODULES = ['fastparquet.speedups', 'fastparquet.cencoding']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 APP_DIR = os.path.normpath(os.path.join(os.getcwd(), '../..'))
